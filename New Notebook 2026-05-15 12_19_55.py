f488e02a18b89a67387bd846415024386f360a89
gutaors@gmail.com <gutaors@gmail.com>
2026-05-15 15:50:59 +0000

    testa se mudança de formato para py funcionou

---
+++ New Notebook 2026-05-15 12_19_55.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+# /// script
+# [tool.databricks.environment]
+# environment_version = "5"
+# ///
+print("test")
+
+# COMMAND ----------
+
+print("testa se include in exports funcionou")
+
+# COMMAND ----------
+
+
